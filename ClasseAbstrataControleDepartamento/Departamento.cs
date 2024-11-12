5317fd36820601f1813f8e2116f33bdd65a17583
Felipe Pretti <felipepretti2001@gmail.com>
2024-11-12 19:56:13 -0300

    att Controle Departamento

--- ClasseAbstrataControleDepartamento/Departamento.cs
+++ ClasseAbstrataControleDepartamento/Departamento.cs
@@ -7,6 +7,28 @@ namespace ClasseAbstrataControleDepartamento
 {
     public class Departamento
     {
+        //atributos
+        private int codigo;
+        private string descricao;
         
+
+        //gets e sets
+        public int Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+
+        //métodos
+        public double calcularFolhaPagamento(int diasUteis)
+        {
+
+        }
     }
 }
